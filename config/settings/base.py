1e0db31e11016876e3a0dc2c121c32ceb705f524
Maria Manzoor <110001812+mariamanzoor814@users.noreply.github.com>
2025-12-23 01:01:56 +0500

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,8 +10,8 @@ from corsheaders.defaults import default_headers, default_methods
 # Load environment variables from .env
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 ENV_PATH = BASE_DIR / ".env"
-if ENV_PATH.exists():
-    load_dotenv(ENV_PATH)
+# if ENV_PATH.exists():
+#     load_dotenv(ENV_PATH)
 
 import environ
 env = environ.Env()
