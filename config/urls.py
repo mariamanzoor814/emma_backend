4c45dd30dcc535029d99d82765479d6f30c1f01a
Maria Manzoor <110001812+mariamanzoor814@users.noreply.github.com>
2025-12-22 23:49:10 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from accounts.social_views import social_start, social_jwt
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,14 +20,18 @@ urlpatterns = [
     # dj-rest-auth (social + session endpoints)
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
+    
 
     path("api/navigation/", include("navigation.urls", namespace="navigation")),
     path("api/content/", include("content.urls", namespace="content")),
+    
+    # Explicitly define social start to diagnose routing issue
+    path("accounts/social-start/<str:provider>/", social_start, name="social_start_root"),
     path("accounts/", include("allauth.urls")),
     path("api/pq/", include("pq_test.urls")),
     path("api/mall/", include("shopping_mall.urls", namespace="shopping_mall")),
-    path('api/msp/', include('msp.urls'))
-
+    path('api/msp/', include('msp.urls')),
+    # path("api/auth/google/", GoogleLogin.as_view(), name="google_login"),
     
 ]
 
