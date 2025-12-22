aec47a6d2b494639b7d1388e9b474e596af47c29
Maria Manzoor <110001812+mariamanzoor814@users.noreply.github.com>
2025-12-23 01:48:26 +0500

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,9 +3,10 @@ from .base import *
 DEBUG = False
 
 # In production you must set ALLOWED_HOSTS via env
-SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
+
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
