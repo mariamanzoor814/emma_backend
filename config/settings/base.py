be1f2bbf8fc6b13d1d29956e58b6f4c9bda9df1f
Maria Manzoor <110001812+mariamanzoor814@users.noreply.github.com>
2025-12-23 10:55:21 +0500

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",  # must be high
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
