0ebf91ddb06cbd1f77b09d00fb3f2c36335ad5c1
Maria Manzoor <110001812+mariamanzoor814@users.noreply.github.com>
2026-01-01 16:20:11 +0500

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,17 +7,21 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://plutusinvestment.com",
-    "https://www.plutusinvestment.com",
-]
+ALLOWED_HOSTS = ["plutusinvestment.com", "www.plutusinvestment.com", "api.plutusinvestment.com"]
+
+CSRF_TRUSTED_ORIGINS = ["https://plutusinvestment.com", "https://www.plutusinvestment.com"]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://plutusinvestment.com",
+#     "https://www.plutusinvestment.com",
+# ]
 
 SESSION_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SAMESITE = "None"
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SOCIALACCOUNT_DEFAULT_REDIRECT_URL = "https://plutusinvestment.com/accounts/google/login/callback/"
-
+LOGIN_REDIRECT_URL = "https://plutusinvestment.com/auth/callback"
 
